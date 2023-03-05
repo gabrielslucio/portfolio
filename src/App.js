1433f1918e8bc7a992f59773454c2051ca12fa6c
Gabriel <gabrielsilvalucio@gmail.com>
2023-03-05 16:00:28 +0000

    Área welcome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Navbar from './components/Navbar/Navbar';
+import Welcome from './container/Welcome/Welcome';
 
 import './App.css';
 import './../src/styles/Body.css';
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Welcome />     
       
     </div>
   );
---
+++ src/container/Welcome/Welcome.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Welcome.scss";
+
+const Welcome = () => {
+
+    return (
+        <div className="app__welcome">
+            <div className="app__welcome-container">
+                <h1>BEM VINDO</h1>
+            </div>
+
+        </div>
+    );
+
+};
+
+export default Welcome;
