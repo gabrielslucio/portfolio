ed0e51ddfe0fa40b6822f45cca8ef30b5303b914
Gabriel <gabrielsilvalucio@gmail.com>
2023-03-05 15:55:05 +0000

    Navbar CSS

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,12 +10,18 @@ const Navbar = () => {
         <div className="app__navbar">
             <div className="app__navbar-container">
                 <div className="app__navbar-logo">
-                    <img src={images.logo} />                   
+                    <a href="home">
+                        <img src={images.logo} />
+                    </a>                                       
                 </div>
                 <div className="app__navbar-navigation">
                     <ul className="app__navbar-ul">
-                        <li className="app__navbar-about"><a href="about">About</a></li>
+                        <li className="app__navbar-about">
+                            <a href="about">About</a>
+                        </li>
+                        <span>|</span>
                         <li className="app__navbar-projects"><a href="projects">Projects</a></li>
+                        <span>|</span>                        
                         <li className="app__navbar-contacts"><a href="contacts">Contacts</a></li>
                     </ul>
                 </div>
